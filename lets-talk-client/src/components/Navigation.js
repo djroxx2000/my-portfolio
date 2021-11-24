2cb48dd8c544c12b9b61f9d2c1c6d7f8edd41d11
Dhananjay Shettigar <dhananjay.shettigar2252000@gmail.com>
2021-11-24 08:25:10 +0530

    navigation bar files created

---
+++ lets-talk-client/src/components/Navigation.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../styles/Navigation.css';
+
+export default function Navigation() {
+	return (
+		<div className="navigation">
+			<div className="nav-top"></div>
+			<div className="nav-core"></div>
+			<div className="nav-right"></div>
+		</div>
+	);
+}
